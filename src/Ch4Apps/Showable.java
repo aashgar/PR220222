13fdb162be2e540dcc5e595d7fc49b25969dc13e
aashgar <aashgar@yahoo.com>
2022-03-12 18:11:02 +0200

    All done,,,

---
+++ src/Ch4Apps/Showable.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ch4Apps;
+
+/**
+ *
+ * @author aashgar
+ */
+public interface Showable {    
+    public final int x=10;
+    public void show(String str);
+    public int sum(int x, int y);
+    
+}
---
+++ src/Ch4Apps2/MainApp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ch4Apps2;
+
+/**
+ *
+ * @author aashgar
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        Rectangle m = new Rectangle(5, 10);
+        
+        System.out.println(m.showData("Perimeter:") + m.getperimeter());
+    
+    }
+}
